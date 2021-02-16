9081660dc11d0ab5d3e394cf95cf241e90ccba10
Alice Bloshenkina <48186115+AliceBloshenkina@users.noreply.github.com>
2021-02-16 16:18:27 +0300

    Update and rename task_1.c to factorila.c

---
+++ tasks/factorila.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int factorial(unsigned long n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main ()
+{
+    unsigned long m;
+    int  n;
+    scanf("%d", &n);
+    if (n < 1 || n > 20 ){
+        printf("Error");
+    }
+    m  = factorial(n);
+
+    printf("Otwet: %lu\n", m);
+    return 0;
+}
